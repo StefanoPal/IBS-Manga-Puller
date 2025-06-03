c3d03f67052e0c0164cd22705db8a3b667f6a422
Stefano Palumbo <61989718+StefanoPal@users.noreply.github.com>
2025-06-03 13:09:59 +0200

    Update htmlibs.py

--- htmlibs.py
+++ htmlibs.py
@@ -12,7 +12,14 @@ import pandas as pd
 
 load_dotenv()
 
-print(os.environ['SMTP_SERVER'], os.environ['SENDER_EMAIL'], os.environ['RECEIVER_EMAIL'], os.environ['ROOT_URL'], os.environ['SEARCH_URL'])
+print(
+    os.environ['SMTP_SERVER'],
+    os.environ['SENDER_EMAIL'],
+    os.environ['RECEIVER_EMAIL'],
+    os.environ['ROOT_URL'],
+    os.environ['SEARCH_URL']
+)
+
 port = 587  # For starttls
 smtp_server = os.environ['SMTP_SERVER']
 sender_email = os.environ['SENDER_EMAIL']
@@ -44,48 +51,36 @@ def telegram_bot_sendtext(bot_message):
     return response.json()
 
 
-# telegram_bot_sendtext("Stefano&company")
 context = ssl.create_default_context()
 page = s.get(os.environ['SEARCH_URL'])
-print(page.text)
 soup = BeautifulSoup(page.text, "html.parser")
-# filename = "nuovi_fum.csv"
-# titles = soup.find("div", class_="cc-listing-items").find_all(class_="cc-product-list-item")
-# for elem in titles:
-# print(elem.find(class_="cc-content-title").a.text)
-# root + elem.find(class_="cc-content-title").a.get("href"))
 engine = create_engine(os.environ['DATABASE_URL'])
 conn = engine.connect()
-# print(titles)
-extractedtitles = []
-#lista2 = []
-lastfumlist = []
 
-# Reads comics and urls that were detected last run through Pandas
-# df = pd.read_csv('/home/pi/Desktop/Ibs/fumetti.csv',
-#                  encoding="utf-8", na_values="NaN", index_col=0)
+extractedtitles = []
+lastfumlist = []
 
 df = pd.read_sql_table('ibs_manga', conn)
 df = df.dropna()
-#df = pd.DataFrame()
-# Moves dataframe in a list with tuples -> (title, url)
 if not df.empty:
     for index, row in df.iterrows():
         # print(row["title"])
         lastfumlist.append((row["title"], row["url"]))
 
 print(df)
-# for div in titles:
-#     lista2.append(div.find(class_="cc-content-title").a.text)
 
 # Gets the number of pages
-numeropag = int(
-    soup.find(class_="cc-content-pages").find("ul").find_all("li", _class="cc-page-number")[-1].text.strip())
-print(numeropag)
+pagesDiv = soup.find(class_="cc-content-pages")
+print(pagesDiv)
+pagesDivUL = pagesDiv.find("ul")
+print(pagesDivUL)
+pagesNumbers = pagesDivUL.find_all("a", class_="cc-page-number")
+print(pagesNumbers)
+maxPageNumber = int(pagesNumbers[-1].text.strip())
+print(maxPageNumber)
 
-#numeropag = int(lista2[0].split()[1])
 # Goes through all the pages, requesting them and putting titles and urls (removing query strings) as tuple (title, url) in the "extractedtitles" list
-for enum in list(range(1, numeropag + 1)):
+for enum in list(range(1, maxPageNumber + 1)):
     page = s.get(f'{os.environ["SEARCH_URL"]}&page={enum}')
     print(page.text)
     soup = BeautifulSoup(page.text, "html.parser")
@@ -101,11 +96,8 @@ for enum in list(range(1, numeropag + 1)):
         tup = (div.a.text, root + url)
         extractedtitles.append(tup)
 
-# print(extractedtitles)
-
 # Converts extracted titles and urls in a pandas dataframe, and saves it as csv
 extractedDataframe = pd.DataFrame(extractedtitles, columns=["title", "url"])
-# extractedDataframe.to_csv('/home/pi/Desktop/Ibs/fumetti.csv', encoding='utf-8')
 affected_rows = extractedDataframe.to_sql(name='ibs_manga', con=engine, if_exists='replace', index=False)
 # print(extractedDataframe)
 print(affected_rows)
@@ -113,15 +105,13 @@ print(affected_rows)
 # same as above, but saves the elements that are present only in the "extractedtitles" list
 newcomicsList = sorted(list(set(extractedtitles) - set(lastfumlist)), key=lambda x: x[0])
 newcomicsDF = pd.DataFrame(newcomicsList, columns=["title", "url"])
-# newcomicsList.to_csv('/home/pi/Desktop/Ibs/Nuovi_fum.csv', encoding='utf-8')
 affected_rows = newcomicsDF.to_sql(name='ibs_manga_new', con=engine, if_exists='replace', index=False)
 
-# print(newcomicsList)
 print(affected_rows)
 
 telegram_messages = []
 telegram_message = "Subject: Fumetti nuovi scontati su ibs\n\n"
-messaggioemail = "Subject: Fumetti nuovi scontati su ibs\n\n"
+email_message = "Subject: Fumetti nuovi scontati su ibs\n\n"
 # Regexs Needed for telegram API to properly accept the message with the Inline URL
 telegramTitleEscape = r"([\_\*\[\]\(\)\~\`\>\#\+\-\=\|\{\}\.\!])"
 telegramUrlEscape = r"([\)\\])"
@@ -131,23 +121,19 @@ if not newcomicsDF.empty:
     for index, row in newcomicsDF.iterrows():
         if row["title"] == 0:
             continue
-        messaggioemail = messaggioemail + \
+        email_message = email_message + \
             row["title"] + " - " + row["url"] + "\n"
         # Builds the telegram message by using MarkdownV2 (https://core.telegram.org/bots/api#formatting-options) and escaping the characters in titles and urls as required
         link_to_append = "[{}]({})\n".format(re.sub(telegramTitleEscape, r'\\\1', row["title"]), re.sub(telegramUrlEscape, r'\\\1', row["url"]))
-        
-        if(len(telegram_message + link_to_append) > 4096):
+
+        if (len(telegram_message + link_to_append) > 4096):
             telegram_messages.append(telegram_message)
             telegram_message = link_to_append
-            
+
         else:
             telegram_message += link_to_append
-        
-        
 telegram_messages.append(telegram_message)
 
-# print(messaggioemail)
-# print(messaggiotel)
 # Sends the message to both the designed email address through Gmail SMTP, and through Telegram Bot API
 if not newcomicsDF.empty:
     with smtplib.SMTP(smtp_server, port) as server:
@@ -155,7 +141,7 @@ if not newcomicsDF.empty:
         server.starttls(context=context)
         server.ehlo()  # Can be omitted
         server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, messaggioemail.encode())
+        server.sendmail(sender_email, receiver_email, email_message.encode())
 
     for message in telegram_messages:
         test = telegram_bot_sendtext(str(message.replace("Subject: ", '')))
